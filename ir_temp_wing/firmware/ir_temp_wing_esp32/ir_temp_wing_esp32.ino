ee36e2453bf248074a749143ed849a325c74f4b0
Matt <matthewj8489@gmail.com>
2020-09-24 07:09:20 -0500

    added bluetooth classic (not BLE)

--- ir_temp_wing/firmware/ir_temp_wing_esp32/ir_temp_wing_esp32.ino
+++ ir_temp_wing/firmware/ir_temp_wing_esp32/ir_temp_wing_esp32.ino
@@ -1,8 +1,17 @@
 #include <Wire.h>
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
 
 void setup() {
   Wire.begin();
-  Serial.begin(9600);
+  Serial.begin(115200);
+  SerialBT.begin("ESP32Temp");
+  Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
 void loop() {
@@ -11,7 +20,8 @@ void loop() {
   while (Wire.available()) {
     char c = Wire.read();
     Serial.print(c);
+    SerialBT.write(c);
   }
 
-  delay(500);
+  delay(2000);
 }
