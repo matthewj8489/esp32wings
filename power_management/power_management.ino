2ae09495af4e27a8f22fb72f697edb36ec1f8af4
Matt <matthewj8489@gmail.com>
2020-09-30 22:07:08 -0500

    adding a module for testing battery power tracking

---
+++ power_management/power_management.ino
@@ -0,0 +1,41 @@
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+#define BAT_VOLT_PIN  A13
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32 Power Manager");
+
+  Serial.println("Here we go again...");
+}
+
+void loop() {
+  int bat_volt = 0;
+  char volt_str[10];
+  int index = 0;
+  int str_len = 0;
+
+  bat_volt = analogRead(BAT_VOLT_PIN);
+  Serial.print("Ok, what the sidewinder car ticker am I reading?? ");
+  Serial.println(bat_volt);
+  bat_volt = bat_volt * 2;
+
+  str_len = sprintf(volt_str, "%d\r\n", bat_volt);
+
+  Serial.print("Here be string: ");
+  Serial.print(volt_str);
+  Serial.print("Chars away..... ");
+  for (index = 0; index < str_len; index++)
+  {
+    Serial.print(volt_str[index]);
+    SerialBT.write(volt_str[index]);
+  }
+  
+  delay(1000);
+}
